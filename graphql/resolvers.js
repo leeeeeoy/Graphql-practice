e386bf7771d50e7e7b2f54ee14fdc8540bc72f0d
leeeeeoy <hoeho18@naver.com>
2021-01-02 18:44:49 +0900

    done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,24 @@
-import { getById, people } from "./db"
+import { getMovies } from "./db"
 
 export const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, { id }) => getById(id)
+        myMovies: (_, { limit, rating }) => getMovies(limit, rating),
     }
 };
 
 export default resolvers;
+
+// export const resolvers = {
+//     Query: {
+//         people: () => people,
+//         person: (_, { id }) => getById(id),
+//         movies: () => getMovies(),
+//         movie: (_, { id }) => getMovieById(id),
+//     },
+//     Mutation: {
+//         addMovie: (_, { name, score }) => addMovie(name, score),
+//         deleteMovie: (_, { id }) => deleteMovie(id),
+//     }
+// };
+
+// export default resolvers;
